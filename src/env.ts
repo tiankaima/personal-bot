1203460bcee15b3bd4af189444379e4454c82f1f
TianKai Ma <tiankaima@163.com>
2024-11-13 03:10:02 +0800

    fix: monkey patch

--- src/env.ts
+++ src/env.ts
@@ -1,22 +1,28 @@
-import { ExecutionContext, KVNamespace, ScheduledController } from '@cloudflare/workers-types/experimental';
+import { ExecutionContext, KVNamespace } from '@cloudflare/workers-types/experimental';
 import { TelegramAPI } from './api/telegram';
 
-export class Env {
+interface Env {
 	DATA: KVNamespace;
 	ENV_BOT_TOKEN: string;
 	ENV_BOT_SECRET: string;
 	ENV_BOT_ADMIN_CHAT_ID: string;
 
-	async load<T = string[]>(key: string, defaultValue: string = '[]'): Promise<T> {
-		const value = await this.DATA.get(key);
-		return JSON.parse(value || defaultValue);
-	}
-
-	async save(key: string, value: any): Promise<void> {
-		await this.DATA.put(key, JSON.stringify(value));
-	}
-
-	get bot(): TelegramAPI {
-		return new TelegramAPI(this.ENV_BOT_TOKEN);
-	}
+	load<T = string[]>(key: string, defaultValue: string): Promise<T>;
+	save(key: string, value: any): Promise<void>;
+	get bot(): TelegramAPI;
 }
+
+(globalThis as any).ExecutionContext.prototype.load = async function <T = string[]>(key: string, defaultValue: string): Promise<T> {
+	const value = await this.DATA.get(key);
+	return JSON.parse(value || defaultValue);
+};
+
+(globalThis as any).ExecutionContext.prototype.save = async function (key: string, value: any): Promise<void> {
+	await this.DATA.put(key, JSON.stringify(value));
+};
+
+Object.defineProperty((globalThis as any).ExecutionContext.prototype, 'bot', {
+	get: function () {
+		return new TelegramAPI(this.ENV_BOT_TOKEN);
+	},
+});
